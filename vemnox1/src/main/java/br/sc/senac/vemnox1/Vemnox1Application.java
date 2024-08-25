83cc3f9f2d1d5b21030c2ebccf93a20b794641ed
Júlia Morsch <juliamorsh17@gmail.com>
2024-08-25 18:33:28 -0300

    first commit.

---
+++ vemnox1/src/main/java/br/sc/senac/vemnox1/Vemnox1Application.java
@@ -0,0 +1,13 @@
+package br.sc.senac.vemnox1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vemnox1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vemnox1Application.class, args);
+	}
+
+}
---
+++ vemnox1/src/test/java/br/sc/senac/vemnox1/Vemnox1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.sc.senac.vemnox1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vemnox1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
