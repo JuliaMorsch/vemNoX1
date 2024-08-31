9dea74f0a9fbf474507b4abf245efb84068b100f
Júlia Morsch <juliamorsh17@gmail.com>
2024-08-31 18:24:42 -0300

    feat: Adicionar classe Partida e CartaNaPartida recebendo os campos da chave composta.

---
+++ vemnox1/src/main/java/br/sc/senac/vemnox1/model/entity/CartaNaPartidaPk.java
@@ -0,0 +1,17 @@
+// package br.sc.senac.vemnox1.model.entity;
+
+// import java.io.Serializable;
+
+// import jakarta.persistence.Column;
+// import jakarta.persistence.Embeddable;
+
+// // Classe com Chave Composta
+// @Embeddable
+// public class CartaNaPartidaPk implements Serializable {
+//     @Column(name = id_carta)
+//     Integer idCarta;
+
+//     @Column(name = "id_partida")
+//     Integer idPartida;
+    
+// }
---
+++ vemnox1/src/main/java/br/sc/senac/vemnox1/model/entity/Partida.java
@@ -0,0 +1,39 @@
+package br.sc.senac.vemnox1.model.entity;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+
+@Entity
+@Data
+public class Partida {
+
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    // @ManyToOne
+    // @JoinColumn(name = "jogador_fk")
+    // private Jogador jogador;
+
+    // @OneToMany(mappedBy = "partida")
+    // private List<CartaNaPartida> cartasJogador;
+
+    private int roundsVencidosJogador;
+    private int roundsVencidosCpu;
+    private int roundsEmpatados;
+    // private Resultado resultado;
+    private LocalDateTime data;
+    private boolean jogouForca;
+    private boolean jogouInteligencia;
+    private boolean jogouVelocidade;
+
+}
