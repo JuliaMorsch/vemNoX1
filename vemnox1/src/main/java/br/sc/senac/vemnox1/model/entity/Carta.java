4c3778e35924054952229a2e3387b1b2566e54ca
Júlia Morsch <juliamorsh17@gmail.com>
2024-09-01 17:22:24 -0300

    feat: Adicionar novo atributo e anotações de validação de campo à entidade Carta.

--- vemnox1/src/main/java/br/sc/senac/vemnox1/model/entity/Carta.java
+++ vemnox1/src/main/java/br/sc/senac/vemnox1/model/entity/Carta.java
@@ -9,6 +9,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
@@ -25,11 +28,27 @@ public class Carta {
     @Size(min = 3, max = 255)
     private String nome;
 
+    @NotBlank
+    @Email
+    private String email;
+
+    @Min(1)
+    @Max(5)
     private int forca;
+
+    @Min(1)
+    @Max(5)
     private int inteligencia;
+
+    @Min(1)
+    @Max(5)
     private int velocidade;
 
     @CreationTimestamp
     private LocalDate dataCadastro;
+
+    public int getTotalAtributos() {
+        return this.forca + this.velocidade + this.inteligencia;
+    }
     
 }
