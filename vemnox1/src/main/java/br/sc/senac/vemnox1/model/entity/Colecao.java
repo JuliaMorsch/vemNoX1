fc5e2eb4320a90761aa9811417fec41c7a44005d
JULIA MORSCH <julia.morsch@alunos.sc.senac.br>
2024-09-03 10:19:26 -0300

    feat: Adicionar entidade coleção.

---
+++ vemnox1/src/main/java/br/sc/senac/vemnox1/model/entity/Colecao.java
@@ -0,0 +1,22 @@
+package br.sc.senac.vemnox1.model.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Colecao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    
+    private String nome;
+    private String cor;
+    private LocalDate dataCriacao;
+}
