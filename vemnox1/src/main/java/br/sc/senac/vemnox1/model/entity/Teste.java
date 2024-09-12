c155a5de1c8aaf352680525c74e97244903332dd
Júlia Morsch <juliamorsh17@gmail.com>
2024-09-11 23:42:28 -0300

    feat: Criar entidade para teste aplicando anotações de validadores.

---
+++ vemnox1/src/main/java/br/sc/senac/vemnox1/model/entity/Teste.java
@@ -0,0 +1,29 @@
+package br.sc.senac.vemnox1.model.entity;
+
+import org.hibernate.annotations.UuidGenerator;
+import org.hibernate.validator.constraints.br.CPF;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Entity
+@Table
+@Data
+public class Teste {
+
+    @Id
+    @UuidGenerator
+    private String uuid;
+
+    @NotBlank(message = "Nome é obrigatório.")
+    @Size(min = 3, max= 255)
+    private String nome;
+
+    @CPF
+    private String cpf;
+
+}
