d9461f04dec89ea32f60e6c312bd84f652a436ac
Júlia Morsch <juliamorsh17@gmail.com>
2024-09-12 23:01:46 -0300

    feat: Sortear seis cartas

--- vemnox1/src/main/java/br/sc/senac/vemnox1/model/repository/CartaRepository.java
+++ vemnox1/src/main/java/br/sc/senac/vemnox1/model/repository/CartaRepository.java
@@ -1,11 +1,16 @@
 package br.sc.senac.vemnox1.model.repository;
 
+import java.util.ArrayList;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.sc.senac.vemnox1.model.entity.Carta;
 
 @Repository
-public interface CartaRepository extends JpaRepository<Carta, Integer> {
-    
+public interface CartaRepository extends JpaRepository<Carta, Integer>, JpaSpecificationExecutor<Carta>{
+
+    @Query(value = "SELECT * FROM carta ORDER BY RAND() limit 6", nativeQuery = true)
+    ArrayList<Carta> sortearSeisCartas();
 }
