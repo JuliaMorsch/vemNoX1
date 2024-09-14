b7b7dacc376de5c0d04e3b275e992e6b73bf72c2
Júlia Morsch <juliamorsh17@gmail.com>
2024-09-14 00:12:25 -0300

    chore: Desabilitei temporariamente o código não utilizado.

--- vemnox1/src/main/java/br/sc/senac/vemnox1/model/entity/Jogador.java
+++ vemnox1/src/main/java/br/sc/senac/vemnox1/model/entity/Jogador.java
@@ -1,16 +1,16 @@
 package br.sc.senac.vemnox1.model.entity;
 
 import java.time.LocalDateTime;
-import java.util.List;
+// import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+// import com.fasterxml.jackson.annotation.JsonBackReference;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+// import jakarta.persistence.OneToMany;
 import lombok.Data;
 
 @Entity
